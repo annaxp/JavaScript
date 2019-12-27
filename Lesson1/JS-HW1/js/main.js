645c80af38e57c227528b8417df56aa46a76a1a3
annaxp <annaxp@ya.ru>
2019-12-27 21:32:30 +0300

    JS-HW1 ver3

--- Lesson1/JS-HW1/js/main.js
+++ Lesson1/JS-HW1/js/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const products = [
-  {id: 1, title: 'Notebook', price: 20000},
+  {id: 1, title: 'Notebook', price: 20000, img: './img/product01.jpg'},
   {id: 2, title: 'Mouse', price: 1500},
   {id: 3, title: 'Keyboard', price: 5000},
   {id: 4, title: 'Gamepad', price: 4500},
@@ -12,22 +12,42 @@ const products = [
 
 ];
 
-const renderProduct = (item, img = 'https://placehold.it/200x150') => {
-  return `<div class="product-item">
-        <img src="${img}" alt="Some img">
-            <h3>${item.title}</h3>
-            <p>${item.price}</p>
-            <button class="by-btn">Добавить в корзину</button>
-          </div>`;
-};
+// const renderProduct = (item, img = 'https://placehold.it/200x150') => {
+//   return `<div class="product-item">
+//         <img src="${img}" alt="Some img">
+//          <div class="desc">
+//             <h3>${item.title}</h3>
+//             <p>${item.price}</p>
+//             <button class="by-btn">Добавить в корзину</button>
+//              </div>
+//           </div>`;
+// };
+
+const renderProduct = (item, img='https://placehold.it/200x150') => `<div class="product-item">
+                <img src="${img}" alt="Some img">
+                <div class="desc">
+                    <h3>${item.title}</h3>
+                    <p>${item.price} \u20bd</p>
+                    <button class="buy-btn">Купить</button>
+                </div>
+            </div>`;
 
 const renderProducts = list => {
-  // const productList = list.map(product => renderProduct(product.title, product.price));
-  const productList = list.map(function (product) {
-    return renderProduct(product.title, product.price)
-  });
-  console.log(productList);
-  document.querySelector('.products').innerHTML = productList;
+  document.querySelector('.products').insertAdjacentHTML('beforeend', list.map(item => renderProduct(item, item.img)).join(''));
 };
 
 renderProducts(products);
+
+
+
+
+// const renderProducts = list => {
+//   // const productList = list.map(product => renderProduct(product.title, product.price));
+//   const productList = list.map(function (product) {
+//     return renderProduct(product.title, product.price)
+//   });
+//   console.log(productList);
+//   document.querySelector('.products').innerHTML = productList;
+// };
+//
+// renderProducts(products);
