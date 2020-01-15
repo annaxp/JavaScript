d048af528ec57c6aa749dea8ca172ac1f71b7321
annaxp <annaxp@ya.ru>
2020-01-15 23:56:30 +0300

    JS-HW2 ver.2

--- Lesson2/JS-HW2/js/main.js
+++ Lesson2/JS-HW2/js/main.js
@@ -15,10 +15,10 @@ class ProductList {
 
   _fetchProducts() {
     this.goods = [
-      {id: 1, title: 'Notebook', price: 45000},
-      {id: 2, title: 'Mouse', price: 3000},
-      {id: 3, title: 'Keyboard', price: 2500},
-      {id: 4, title: 'Gamepad', price: 1500},
+      {id: 1, title: 'Notebook', price: 20000, img: './img/401_1.jpg'},
+      {id: 2, title: 'Mouse', price: 1500, img: './img/202_2.jpg'},
+      {id: 3, title: 'Keyboard', price: 5000, img: './img/301_2.jpg'},
+      {id: 4, title: 'Gamepad', price: 1500, img: 'https://placehold.it/200x150'}, // костыль
     ]
   }
 
@@ -34,11 +34,11 @@ class ProductList {
 }
 
 class ProductItem {
-  constructor(product, img = 'https://placehold.it/200x150') {
+  constructor(product, img = 'https://placehold.it/200x150') { // не срабатывает значение по умолчанию после иср=правления в строке 41
     this.title = product.title;
     this.price = product.price;
     this.id = product.id;
-    this.img = img;
+    this.img = product.img; // исправила так, как не считывает картинку с goods
   }
 
   render() {
@@ -58,32 +58,32 @@ console.log(list);
 
 
 // НИЖЕ урок 1
-
-const products = [
-  {id: 1, title: 'Notebook', price: 20000, img: './img/401_1.jpg'},
-  {id: 2, title: 'Mouse', price: 1500, img: './img/202_2.jpg'},
-  {id: 3, title: 'Keyboard', price: 5000, img: './img/301_2.jpg'},
-  {id: 4, title: 'Gamepad', price: 4500},
-  {id: 5, title: 'Notebook2', price: 100},
-  {id: 6, title: 'Notebook3', price: 4500},
-  {id: 7, title: 'Gamepad3', price: 100},
-  {id: 8, title: 'Gamepad4', price: 100},
-
-];
-
-
-const renderProduct = (item, img='https://placehold.it/200x150') => `<div class="product-item">
-                <img src="${img}" alt="Some img">
-                <div class="desc">
-                    <h3>${item.title}</h3>
-                    <p>${item.price} \u20bd</p>
-                    <button class="buy-btn">Купить</button>
-                </div>
-            </div>`;
-
-const renderProducts = list => {
-  document.querySelector('.products').insertAdjacentHTML('beforeend', list.map(item => renderProduct(item, item.img)).join(''));
-};
-
-renderProducts(products);
+//
+// const products = [
+//   {id: 1, title: 'Notebook', price: 20000, img: './img/401_1.jpg'},
+//   {id: 2, title: 'Mouse', price: 1500, img: './img/202_2.jpg'},
+//   {id: 3, title: 'Keyboard', price: 5000, img: './img/301_2.jpg'},
+//   {id: 4, title: 'Gamepad', price: 4500},
+//   {id: 5, title: 'Notebook2', price: 100},
+//   {id: 6, title: 'Notebook3', price: 4500},
+//   {id: 7, title: 'Gamepad3', price: 100},
+//   {id: 8, title: 'Gamepad4', price: 100},
+//
+// ];
+//
+//
+// const renderProduct = (item, img='https://placehold.it/200x150') => `<div class="product-item">
+//                 <img src="${img}" alt="Some img">
+//                 <div class="desc">
+//                     <h3>${item.title}</h3>
+//                     <p>${item.price} \u20bd</p>
+//                     <button class="buy-btn">Купить</button>
+//                 </div>
+//             </div>`;
+//
+// const renderProducts = list => {
+//   document.querySelector('.products').insertAdjacentHTML('beforeend', list.map(item => renderProduct(item, item.img)).join(''));
+// };
+//
+// renderProducts(products);
 
